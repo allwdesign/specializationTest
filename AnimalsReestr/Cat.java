01d8149ef3d329b441b203d6d64ca3ddaadbf781
Natalia Kuzmenko <allwdesign@gmail.com>
2023-07-06 15:07:51 +0300

    Add initial structure

---
+++ AnimalsReestr/Cat.java
@@ -0,0 +1,11 @@
+package AnimalsReestr;
+
+import java.util.Date;
+
+public class Cat extends Pets{
+
+    public Cat(String name, String commands, Date birthday, Boolean gender, String size, String ownerFio) {
+        super(name, commands, birthday, gender, size, ownerFio);
+    }
+    
+}
---
+++ AnimalsReestr/Dog.java
@@ -0,0 +1,11 @@
+package AnimalsReestr;
+
+import java.util.Date;
+
+public class Dog extends Pets{
+
+    public Dog(String name, String commands, Date birthday, Boolean gender, String size, String ownerFio) {
+        super(name, commands, birthday, gender, size, ownerFio);
+    }
+    
+}
